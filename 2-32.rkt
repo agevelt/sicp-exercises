9b13431969e4286f9b4b86cb35ef4b808de5a6a7
Audun Gevelt <a.gevelt@gmail.com>
2016-12-15 08:26:23 +0100

    Chapter 2.2.2

--- 2-32.rkt
+++ 2-32.rkt
@@ -8,7 +8,7 @@
                             (cons (car s) x)) rest)))))
 
 ;; It's a kind of magic.
-;; It works because we are building a binary tree structure starting with a root and two branches with nodes (3) and '(). On every new level we cons the "parent" element on the node to the left branch, and pass along the current node to the right branch. This way we are continually passing along all new and current subsets to the next level, where we construct all new possible subsets that include (car s).
+;; It works because we are building a binary tree structure starting with a root and two branches with nodes (3) and '(). On every new level we cons the "parent" element on the node to the left branch, and pass along the parent node to the right branch. This way we are continually passing along all new and current subsets to the next level, where we construct all new possible subsets that include (car s).
 
 
 ;;                                     /\
