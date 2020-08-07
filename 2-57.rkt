1f5010fcb4d77a92ac6a228b0b463d1a5ddd3daf
Audun <a.gevelt@gmail.com>
2020-08-07 14:06:02 +0200

    Chapter 2.3.2

---
+++ 2-57.rkt
@@ -0,0 +1,89 @@
+#lang sicp
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+            (make-product (multiplier exp)
+                          (deriv (multiplicand exp) var))
+            (make-product (deriv (multiplier exp) var)
+                          (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product (exponent exp)
+                       (make-exponentiation (base exp)
+                                            (- (exponent exp) 1))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (=number? exp num)
+  (and (number? exp) (eq? exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) (cadr s))
+
+(define (augend s)
+  (cond ((or (null? s) (eq? (length s) 1) (eq? (length s) 2)) 0)
+        ((eq? (length s) 3) (caddr s))
+        (else (make-sum (caddr s) (cadddr s)))))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+
+(define (multiplicand p)
+  (cond ((or (null? p) (eq? (length p) 1) (eq? (length p) 2)) 1)
+        ((eq? (length p) 3) (caddr p))
+        (else (make-product (caddr p) (cadddr p)))))
+
+(define (exponentiation? exp)
+  (eq? (car exp) '**))
+
+(define (base e) (cadr e))
+
+(define (exponent e) (caddr e))
+
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+        ((=number? e 1) b)
+        ((and (number? b) (number? e)) (* b (make-exponentiation b (- e 1))))
+        (else (list '** b e))))
+
+
+
+(deriv '(+ x 3) 'x)
+
+(deriv '(* x y) 'x)
+
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+;; test
+(deriv '(* x y (+ x 3)) 'x)
+
+
